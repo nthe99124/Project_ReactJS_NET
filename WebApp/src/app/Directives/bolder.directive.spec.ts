b90859571b83f362a262b25d57685aec4c57560c
nthe99124 <72652449+nthe99124@users.noreply.github.com>
2022-05-09 00:07:20 +0700

    Change webApp to ReactJS

--- WebApp/src/app/Directives/bolder.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BolderDirective } from './bolder.directive';
-
-describe('BolderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BolderDirective();
-    expect(directive).toBeTruthy();
-  });
-});
